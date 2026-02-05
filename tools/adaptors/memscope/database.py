174e808c2fa3b0c3c02e905703650cf936e282ed
codeasier <liuyekang@huawei.com>
2026-02-05 23:36:25 +0800

    增加memscope定义包

---
+++ tools/adaptors/memscope/database.py
@@ -0,0 +1,121 @@
+"""
+-------------------------------------------------------------------------
+This file is part of the MindStudio project.
+Copyright (c) 2026 Huawei Technologies Co.,Ltd.
+
+MindStudio is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+
+         http://license.coscl.org.cn/MulanPSL2
+
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+-------------------------------------------------------------------------
+"""
+from typing import List
+
+from util.sqlite_meta import SqliteColumn, SqliteTable, SqliteDB
+from .defs import (
+    DumpEventFieldDefs,
+    PythonTraceEventFieldDefs,
+    MemoryBlockFieldDefs,
+    MemoryAllocationFieldDefs
+)
+
+_MEMSCOPE_DUMP_TABLE_COLUMNS: List[SqliteColumn] = [
+    SqliteColumn(name=DumpEventFieldDefs.ID, data_type=int, primary_key=True, autoincrement=True),
+    SqliteColumn(DumpEventFieldDefs.EVENT),
+    SqliteColumn(DumpEventFieldDefs.EVENT_TYPE),
+    SqliteColumn(DumpEventFieldDefs.NAME),
+    SqliteColumn(DumpEventFieldDefs.TIMESTAMP_NS, data_type=int),
+    SqliteColumn(DumpEventFieldDefs.PID, data_type=int),
+    SqliteColumn(DumpEventFieldDefs.TID, data_type=int),
+    SqliteColumn(DumpEventFieldDefs.DEVICE_ID),
+    SqliteColumn(DumpEventFieldDefs.PTR),
+    SqliteColumn(DumpEventFieldDefs.ATTR),
+    SqliteColumn(DumpEventFieldDefs.CALLSTACK_PYTHON),
+    SqliteColumn(DumpEventFieldDefs.CALLSTACK_C)
+]
+
+_PYTHON_TRACE_TABLE_COLUMNS: List[SqliteColumn] = [
+    SqliteColumn(PythonTraceEventFieldDefs.FUNC_INFO),
+    SqliteColumn(PythonTraceEventFieldDefs.START_TIME_NS),
+    SqliteColumn(PythonTraceEventFieldDefs.END_TIME_NS),
+    SqliteColumn(PythonTraceEventFieldDefs.TID, data_type=int),
+    SqliteColumn(PythonTraceEventFieldDefs.PID, data_type=int)
+]
+
+_MEMORY_BLOCK_TABLE_COLUMNS: List[SqliteColumn] = [
+    SqliteColumn(MemoryBlockFieldDefs.ID, data_type=int, primary_key=True),
+    SqliteColumn(MemoryBlockFieldDefs.DEVICE_ID),
+    SqliteColumn(MemoryBlockFieldDefs.ADDR),
+    SqliteColumn(MemoryBlockFieldDefs.SIZE, data_type=int),
+    SqliteColumn(MemoryBlockFieldDefs.START_TIME_NS, data_type=int),
+    SqliteColumn(MemoryBlockFieldDefs.END_TIME_NS, data_type=int),
+    SqliteColumn(MemoryBlockFieldDefs.EVENT_TYPE),
+    SqliteColumn(MemoryBlockFieldDefs.OWNER),
+    SqliteColumn(MemoryBlockFieldDefs.ATTR),
+    SqliteColumn(MemoryBlockFieldDefs.PID, data_type=int),
+    SqliteColumn(MemoryBlockFieldDefs.TID, data_type=int),
+    SqliteColumn(MemoryBlockFieldDefs.STREAM, data_type=int)
+]
+
+_MEMORY_ALLOCATION_TABLE_COLUMNS: List[SqliteColumn] = [
+    SqliteColumn(MemoryAllocationFieldDefs.ID, data_type=int, primary_key=True),
+    SqliteColumn(MemoryAllocationFieldDefs.TIME_NS, data_type=int),
+    SqliteColumn(MemoryAllocationFieldDefs.TOTAL_SIZE, data_type=int),
+    SqliteColumn(MemoryAllocationFieldDefs.OPTIMIZED, data_type=int),
+    SqliteColumn(MemoryAllocationFieldDefs.DEVICE_ID),
+    SqliteColumn(MemoryAllocationFieldDefs.EVENT_TYPE)
+]
+
+
+class MemScopeDb(SqliteDB):
+    DUMP_TABLE_NAME = "leaks_dump"
+    MEMORY_BLOCK_TABLE_NAME = "memory_block"
+    MEMORY_ALLOC_TABLE_NAME = "memory_allocation"
+    PYTHON_TRACE_TABLE_NAME_PREFIX = "python_trace"
+
+    def __init__(self, path: str):
+        super().__init__(path, auto_create=True)
+        self.create_dump_table()
+        self.create_allocation_table()
+        self.create_memory_block_table()
+
+    def create_dump_table(self):
+        self.create_table(SqliteTable(MemScopeDb.DUMP_TABLE_NAME, _MEMSCOPE_DUMP_TABLE_COLUMNS),
+                          delete_if_exists=True)
+
+    def create_memory_block_table(self):
+        self.create_table(SqliteTable(MemScopeDb.MEMORY_BLOCK_TABLE_NAME, _MEMORY_BLOCK_TABLE_COLUMNS),
+                          delete_if_exists=True)
+
+    def create_allocation_table(self):
+        self.create_table(SqliteTable(MemScopeDb.MEMORY_ALLOC_TABLE_NAME, _MEMORY_ALLOCATION_TABLE_COLUMNS),
+                          delete_if_exists=True)
+
+    def create_python_trace_table(self, pid: int):
+        self.create_table(SqliteTable(MemScopeDb.get_python_trace_table_name(pid), _PYTHON_TRACE_TABLE_COLUMNS),
+                          delete_if_exists=True)
+
+    def get_dump_table(self):
+        return self.get_table_by_name(MemScopeDb.DUMP_TABLE_NAME)
+
+    def get_allocation_table(self):
+        return self.get_table_by_name(MemScopeDb.MEMORY_ALLOC_TABLE_NAME)
+
+    def get_memory_block_table(self):
+        return self.get_table_by_name(MemScopeDb.MEMORY_BLOCK_TABLE_NAME)
+
+    def get_python_trace_table(self, pid: int):
+        python_trace_table_name = MemScopeDb.get_python_trace_table_name(pid)
+        if not self.is_table_exists(python_trace_table_name):
+            self.create_python_trace_table(pid)
+        return self.get_table_by_name(python_trace_table_name)
+
+    @classmethod
+    def get_python_trace_table_name(cls, pid: int):
+        return f"{cls.PYTHON_TRACE_TABLE_NAME_PREFIX}_{pid}"
---
+++ tools/adaptors/memscope/entities.py
@@ -0,0 +1,138 @@
+"""
+-------------------------------------------------------------------------
+This file is part of the MindStudio project.
+Copyright (c) 2026 Huawei Technologies Co.,Ltd.
+
+MindStudio is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+
+         http://license.coscl.org.cn/MulanPSL2
+
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
+-------------------------------------------------------------------------
+"""
+import json
+from dataclasses import dataclass
+
+from .defs import (
+    DumpEventFieldDefs,
+    PythonTraceEventFieldDefs,
+    MemoryBlockFieldDefs,
+    MemoryAllocationFieldDefs
+)
+
+
+@dataclass
+class MallocFreeEventAttr:
+    addr: str
+    size: str
+    used: str
+    total: str
+    stream_ptr: str = "N/A"
+    owner: str = "PTA"
+
+
+@dataclass
+class MemoryEvent:
+    _id: int
+    event: str
+    event_type: str
+    name: str
+    timestamp: int
+    pid: int
+    tid: int
+    did: int
+    ptr: str
+    attr: dict
+    callstack_c: str = ""
+    callstack_py: str = ""
+
+    def to_dict(self):
+        return {
+            DumpEventFieldDefs.ID: self._id,
+            DumpEventFieldDefs.EVENT: self.event,
+            DumpEventFieldDefs.EVENT_TYPE: self.event_type,
+            DumpEventFieldDefs.NAME: self.name,
+            DumpEventFieldDefs.TIMESTAMP_NS: self.timestamp,
+            DumpEventFieldDefs.PID: self.pid,
+            DumpEventFieldDefs.TID: self.tid,
+            DumpEventFieldDefs.DEVICE_ID: self.did,
+            DumpEventFieldDefs.PTR: self.ptr,
+            DumpEventFieldDefs.ATTR: json.dumps(self.attr),
+            DumpEventFieldDefs.CALLSTACK_C: self.callstack_c,
+            DumpEventFieldDefs.CALLSTACK_PYTHON: self.callstack_py
+        }
+
+
+@dataclass
+class PythonTraceEvent:
+    func_info: str
+    start_time_ns: int
+    end_time_ns: int
+    tid: int
+    pid: int
+
+    def to_dict(self):
+        return {
+            PythonTraceEventFieldDefs.FUNC_INFO: self.func_info,
+            PythonTraceEventFieldDefs.START_TIME_NS: self.start_time_ns,
+            PythonTraceEventFieldDefs.END_TIME_NS: self.end_time_ns,
+            PythonTraceEventFieldDefs.TID: self.tid,
+            PythonTraceEventFieldDefs.PID: self.pid
+        }
+
+
+@dataclass
+class MemoryBlock:
+    block_id: int
+    device_id: int
+    addr: str
+    size: int
+    start_time_ns: int
+    end_time_ns: int
+    event_type: str
+    owner: str
+    attr: dict
+    pid: int
+    tid: int
+    stream: int
+
+    def to_dict(self):
+        return {
+            MemoryBlockFieldDefs.ID: self.block_id,
+            MemoryBlockFieldDefs.DEVICE_ID: self.device_id,
+            MemoryBlockFieldDefs.ADDR: self.addr,
+            MemoryBlockFieldDefs.SIZE: self.size,
+            MemoryBlockFieldDefs.START_TIME_NS: self.start_time_ns,
+            MemoryBlockFieldDefs.END_TIME_NS: self.end_time_ns,
+            MemoryBlockFieldDefs.EVENT_TYPE: self.event_type,
+            MemoryBlockFieldDefs.OWNER: self.owner,
+            MemoryBlockFieldDefs.ATTR: json.dumps(self.attr),
+            MemoryBlockFieldDefs.PID: self.pid,
+            MemoryBlockFieldDefs.TID: self.tid,
+            MemoryBlockFieldDefs.STREAM: self.stream,
+        }
+
+
+@dataclass
+class MemoryAllocation:
+    alloc_id: int
+    timestamp: int
+    total_size: int
+    device_id: int
+    event_type: str
+    optimized: int = 0
+
+    def to_dict(self):
+        return {
+            MemoryAllocationFieldDefs.ID: self.alloc_id,
+            MemoryAllocationFieldDefs.TIME_NS: self.timestamp,
+            MemoryAllocationFieldDefs.TOTAL_SIZE: self.total_size,
+            MemoryAllocationFieldDefs.OPTIMIZED: self.optimized,
+            MemoryAllocationFieldDefs.DEVICE_ID: self.device_id,
+            MemoryAllocationFieldDefs.EVENT_TYPE: self.event_type
+        }
