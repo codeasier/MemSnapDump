00130d33cf452801b829263a6eb2904cf6a5b785
codeasier <liuyekang@huawei.com>
2026-02-08 11:08:30 +0800

    增加sys.path以适配pyinstaller打包

--- tools/dump2db.py
+++ tools/dump2db.py
@@ -1,4 +1,13 @@
-from .adaptors.snapshot2db import main
+import os
+import sys
+
+# 动态添加项目根目录到 Python 搜索路径
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir)
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
+
+from tools.adaptors.snapshot2db import main
 
 if __name__ == '__main__':
     main()
